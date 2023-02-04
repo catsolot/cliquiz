a7cc2d8207af535493c73b68d6f7efe1ffb6336f
Milo Craun <milocraun@gmail.com>
2023-02-04 11:37:53 -0500

    maybe working header

---
+++ db.h
@@ -0,0 +1,14 @@
+#ifndef DB
+#define DB
+#include <stdio.h>
+#include <sqlite3.h>
+
+struct db_response {
+    char* name;
+    char* question;
+    char* hints;
+};
+
+struct db_response get_response();
+
+#endif
