e4913d35b648c64fae6d6b6b8652b2d7a0decb85
Milo Craun <milocraun@gmail.com>
2023-02-04 14:03:17 -0500

    database works?

--- db.h
+++ db.h
@@ -1,14 +1,18 @@
 #ifndef DB
 #define DB
 #include <stdio.h>
+#include <string.h>
 #include <sqlite3.h>
+#include <time.h>
+#include <stdlib.h>
+#define DB_LENGTH 2
 
 struct db_response {
-    char* name;
-    char* question;
-    char* hints;
+    const char* answer;
+    const char* question;
+    const char* hint;
 };
 
 struct db_response* get_response(sqlite3 * db);
-
+sqlite3 * init_database(char* filename);
 #endif
